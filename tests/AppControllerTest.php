ceb6fb4b4e7d82981756a7f5fa2c66298a3520d4
Thomas <marcanon45@laposte.net>
2020-04-15 17:41:05 +0200

    --

--- tests/AppControllerTest.php
+++ tests/AppControllerTest.php
@@ -6,10 +6,28 @@ use PHPUnit\Framework\TestCase;
 
 class AppControllerTest extends TestCase
 {
-    public function testSomething()
+    public function testTrue()
     {
         $this->assertTrue(true);
+    }
 
-        $this->assertTrue(true);
+    public function testFalse()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testFalse2()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testFalse3()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testFalse4()
+    {
+        $this->assertFalse(false);
     }
 }
